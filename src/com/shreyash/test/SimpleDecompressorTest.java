b70ea5ae0f6db002bf476a69fc5f51f41c90ab86
ShreyashPatodia <spatodia@student.unimelb.edu.au>
2017-09-29 12:57:34 +1000

    Testing compression and decompression formally

--- src/com/shreyash/test/SimpleDecompressorTest.java
+++ src/com/shreyash/test/SimpleDecompressorTest.java
@@ -7,6 +7,12 @@ import com.shreyash.main.decode.SimpleDecompressor;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Tests written for the decompressor.
@@ -14,8 +20,14 @@ import java.io.IOException;
 public class SimpleDecompressorTest {
 
     @Test
-    public void testRead() throws IOException {
+    public void testCompressionAndDecompression() throws IOException {
         SimpleDecompressor decompressor = new SimpleDecompressor();
-        decompressor.decode("tester-compressed.txt");
+        decompressor.decode("test-files/tester-compressed.txt");
+        Path original = Paths.get("test-files/tester.txt");
+        Path reconstructed = Paths.get("test-files/tester-reconstructed.txt");
+        byte[] f1 = Files.readAllBytes(original);
+        byte[] f2 = Files.readAllBytes(reconstructed);
+        assertEquals(true, Arrays.equals(f1, f2));
     }
+    
 }
